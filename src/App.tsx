46122aac0957418e69432385b845ba2b8a8d0778
lavinia <lav.ogawa@gmail.com>
2026-04-01 10:37:36 -0300

    Primeiro commit após importar ZIP

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-zinc-900">
+      <div className="rounded-2xl bg-zinc-800 p-10 shadow-xl">
+        <h1 className="text-4xl font-bold text-blue-400">
+          Tailwind funcionando
+        </h1>
+        <p className="mt-4 text-zinc-300">
+          Seu projeto React + Vite + Tailwind está pronto.
+        </p>
+      </div>
+    </div>
+  )
+}
