afe69dd4f679501f0c5f262ac380fe128087dd38
lavinia <lav.ogawa@gmail.com>
2026-04-15 19:37:59 -0300

    fix: Vite

--- frontend/src/pages/public/Cart.tsx
+++ frontend/src/pages/public/Cart.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button"
-import { Trash2, ShoppingBag, ShieldCheck } from "lucide-react"
+import { Trash2, ShoppingBag } from "lucide-react"
 import { Link } from "react-router-dom"
 import { useCartStore } from "@/stores/useCartStore"
 import { cn } from "@/lib/utils"
